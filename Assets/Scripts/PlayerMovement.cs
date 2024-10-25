11ca09fe5f6e568104a04ee8c425989a5cc953ec
stemguest <stemguest@itcs.ccny.lan>
2024-10-25 15:59:01 -0400

    10/25/2024

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,5 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rigidbody2D;
 
+    private float _xSpeed;
+    private float _ySpeed;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private 
 }
