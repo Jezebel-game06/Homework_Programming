fef5ad6904682673e0bffade4156cefaf439148b
Jezebel-game06 <jezebel.macias@ellisprep.com>
2024-11-08 15:56:43 -0500

    11/8/2024

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -3,4 +3,9 @@ using UnityEngine;
 public class PlayerRotation : MonoBehaviour
 {
 
+    private Camera _camera;
+
+    private string CameraName = "Game_Camera";
+
+    private Vector3 _positionMouse;
 }
